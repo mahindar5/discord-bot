512b9af447ec767d163d234788bb35826dc6b00b
mahindar5 <mahindar_527@yahoo.com>
2024-03-04 11:47:22 -0800

    Fix location.href in help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -15,5 +15,5 @@ export const data = new SlashCommandBuilder()
 			.setDescription('Info about the server'));
 
 export async function execute(interaction: CommandInteraction) {
-	await interaction.reply(`help server reply from ${location.href}`);
+	await interaction.reply(`help server reply from ${location?.href}`);
 }
