83fe38246a5ae9318965bd0109496e8173efb62b
mahindar5 <mahindar_527@yahoo.com>
2024-03-05 13:54:38 -0800

    Update error reporting channel ID

--- src/constants/CineplexChannelId.ts
+++ src/constants/CineplexChannelId.ts
@@ -1,4 +1,4 @@
 const availableBookingsChannelId = '1214654281127563325';
-const errorReportingChannelId = '1214660749428002948';
+const errorReportingChannelId = '1214692489336721439';
 export { availableBookingsChannelId, errorReportingChannelId };
 
